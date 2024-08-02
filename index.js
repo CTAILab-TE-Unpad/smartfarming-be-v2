931bd569c171ee232cb99e7166852b19163cac1f
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2024-08-02 21:27:15 +0700

    limit api anomaly jadi 100

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.get("/anomalyDetection", (req, res) => {
         sensor_name: req.query.sensor_name
     })
     .sort({ createdAt: -1 })
-    .limit(150)
+    .limit(100)
     .then((data) => {
         console.log('data', data);
         return res.json(data);
